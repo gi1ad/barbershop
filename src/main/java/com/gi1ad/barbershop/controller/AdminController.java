7aa9e1498c6f21357dc7cb226c559dbf26cbcf07
gi1ad <wowbk1604>
2019-01-18 16:31:10 +0300

    admin controller fixed, login page improvements

--- src/main/java/com/gi1ad/barbershop/controller/AdminController.java
+++ src/main/java/com/gi1ad/barbershop/controller/AdminController.java
@@ -2,18 +2,33 @@ package com.gi1ad.barbershop.controller;
 
 
 import com.gi1ad.barbershop.entity.News;
+import com.gi1ad.barbershop.service.NewsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AdminController {
 
-    @RequestMapping("/admin")
-    public String admin(Model model){
+    @Autowired
+    NewsServiceImpl service;
+
+    @GetMapping("/admin")
+    public String admin(Model model) {
         model.addAttribute("news", new News());
         return "admin_page";
     }
 
+    @PostMapping("/admin")
+    public String createNews(News news){
+        service.save(news);
+        return "redirect/admin_page";
+    }
+
 
 }
+
+
+
