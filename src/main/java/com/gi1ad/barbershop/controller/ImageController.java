aaf2bcb68f9957a58b6dce25e6bdeceeabd5073d
gi1ad <wowbk1604>
2019-01-28 15:05:24 +0300

    image controller impl

---
+++ src/main/java/com/gi1ad/barbershop/controller/ImageController.java
@@ -0,0 +1,35 @@
+package com.gi1ad.barbershop.controller;
+
+
+import com.gi1ad.barbershop.entity.News;
+import com.gi1ad.barbershop.service.NewsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Controller
+@RequestMapping("/images")
+public class ImageController {
+
+    @Autowired
+    NewsServiceImpl service;
+
+    @RequestMapping("/{id}")
+    public void getImage(@PathVariable long id, HttpServletResponse response){
+        response.setContentType("image/png");
+        News news = (News)service.getById(id);
+        if(news != null){
+            try {
+                response.getOutputStream().write(news.getNewsImage());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } {
+
+        }
+    }
+}
---
+++ src/main/java/com/gi1ad/barbershop/controller/NewsController.java
@@ -0,0 +1,23 @@
+package com.gi1ad.barbershop.controller;
+
+
+import com.gi1ad.barbershop.service.NewsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class NewsController {
+
+    @Autowired
+    NewsServiceImpl service;
+
+    @RequestMapping(method = RequestMethod.GET,value = "/news")
+    public String getNews(Model model){
+        model.addAttribute("news",service.getAll());
+        return "news_page";
+    }
+
+}
